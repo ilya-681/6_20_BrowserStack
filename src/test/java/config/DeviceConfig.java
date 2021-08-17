da3e95d88eaa23d941b8121552e2a1c0fcb956e4
ilya <ilya-681@ya.ru>
2021-08-17 22:32:32 +0300

    Rename properties

--- src/test/java/config/DeviceConfig.java
+++ src/test/java/config/DeviceConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/browserStack.properties",
+        "classpath:config/local.properties",
         "classpath:config/${env}.properties"
 })
 public interface DeviceConfig extends Config {
